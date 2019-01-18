699bd02a6471c2b055bbeb5a8a8b30ad7c426d61
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2019-01-18 23:42:21 +0100

    get id property controller generic

--- SpeedRun.ControllerGeneric/Controller/ControllerGeneric.cs
+++ SpeedRun.ControllerGeneric/Controller/ControllerGeneric.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using SpeedRun.ServiceGeneric.Interface;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace SpeedRun.ControllerGeneric
 {
@@ -23,10 +25,8 @@ namespace SpeedRun.ControllerGeneric
         [HttpGet("{id}")]
         public virtual T Get(Guid id)
         {
-            // TODO : IMPLEMENTER
-            // Il faut que cette méthode du controller return l'objet T en fonction de l'id, qu'on a en paramètre
-            // tu peux accéder a ce point d'api via localhost:5000/api/nomdeController/id
-            return null;
+            PropertyInfo _idProperty = typeof(T).GetProperty("Id");
+            return service.Get(x => (Guid)_idProperty.GetValue(x) == id);
         }
 
         [HttpPost]
