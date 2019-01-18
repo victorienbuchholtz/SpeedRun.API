227aa134bc2aeefac8c852715c190aaf093e49eb
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2019-01-18 20:47:20 +0100

    todo get id controller generic

--- SpeedRun.ControllerGeneric/Controller/ControllerGeneric.cs
+++ SpeedRun.ControllerGeneric/Controller/ControllerGeneric.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using SpeedRun.ServiceGeneric.Interface;
 using System.Collections.Generic;
 
@@ -17,6 +18,15 @@ namespace SpeedRun.ControllerGeneric
         public virtual List<T> GetAll()
         {
             return service.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public virtual T Get(Guid id)
+        {
+            // TODO : IMPLEMENTER
+            // Il faut que cette méthode du controller return l'objet T en fonction de l'id, qu'on a en paramètre
+            // tu peux accéder a ce point d'api via localhost:5000/api/nomdeController/id
+            return null;
         }
 
         [HttpPost]
