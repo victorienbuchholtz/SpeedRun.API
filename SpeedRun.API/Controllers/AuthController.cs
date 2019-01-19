680d3d1fb172b7a963d5107eb6de13da3d59933f
Victorien Buchholtz <victorienbuchholtz@gmail.com>
2019-01-19 15:29:26 +0100

    Create user after successful authentication with GitHub

--- SpeedRun.API/Controllers/AuthController.cs
+++ SpeedRun.API/Controllers/AuthController.cs
@@ -1,17 +1,42 @@
-﻿using System.Threading.Tasks;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using SpeedRun.ControllerGeneric;
+using SpeedRun.Models.Models;
+using SpeedRun.Services.Interfaces;
 
 namespace SpeedRun.API.Controllers
 {
     [Route("auth/")]
-    public class AuthController : Controller
+    public class AuthController : ControllerGeneric<User>
     {
+        public AuthController(IUserService service) : base(service)
+        {
+        }
+
         [Route("signin")]
-        public IActionResult SignIn(string returnUrl = "/")
+        public IActionResult SignIn(string returnUrl = "/auth/signinconfirmed")
         {
             return Challenge(new AuthenticationProperties { RedirectUri = returnUrl });
+        }
+
+        [Route("signinconfirmed")]
+        public IActionResult SignInConfirmed()
+        {
+            User user = new User();
+            if (User.Identity.IsAuthenticated)
+            {
+                user.FirstName = User.FindFirst(c => c.Type == ClaimTypes.Name)?.Value;
+                user.AvatarUrl = User.FindFirst(c => c.Type == "urn:github:avatar")?.Value;
+                user.Email = User.FindFirst(c => c.Type == "urn:github:email")?.Value;
+                user.UserName = User.FindFirst(c => c.Type == "urn:github:login")?.Value;
+
+                return RedirectToRoute("http://localhost:4200/", user);
+            }
+
+            return BadRequest("Access denied");
         }
 
         [Route("signout")]
