9a2edb8a2d58bec1ebf6b8f89999af1bc34a12d4
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2019-01-20 20:06:14 +0100

    add migration for gitan

---
+++ SpeedRun.Models/Migrations/20190120190532_AddGithubIdToUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpeedRun.Models.Migrations
+{
+    public partial class AddGithubIdToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IDGitHub",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IDGitHub",
+                table: "AspNetUsers");
+        }
+    }
+}
