920a0d03e0402f4bc97f8ab7993ed4e3b1a04759
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2019-01-21 16:39:42 +0100

    inventoryOp controller service repo

---
+++ SpeedRun.API/Controllers/InventoryOperationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpeedRun.ControllerGeneric;
+using SpeedRun.Models.Models;
+using SpeedRun.Services.Interfaces;
+
+namespace SpeedRun.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class InventoryOperationController : ControllerGeneric<InventoryOperation>
+    {
+        private readonly IInventoryOperationService _inventoryOperationService;
+
+        public InventoryOperationController(IInventoryOperationService service) : base(service)
+        {
+            _inventoryOperationService = service;
+        }
+
+    }
+}
---
+++ SpeedRun.Services/Services/InventoryOperationService.cs
@@ -0,0 +1,16 @@
+﻿using SpeedRun.Models.Models;
+using SpeedRun.RepositoryGeneric.Interface;
+using SpeedRun.ServiceGeneric;
+using SpeedRun.Services.Interfaces;
+
+namespace SpeedRun.Services.Services
+{
+    public class InventoryOperationService : ServiceGeneric<InventoryOperation>, IInventoryOperationService
+    {
+        public InventoryOperationService(IRepositoryGeneric<InventoryOperation> repo) : base(repo)
+        {
+        }
+
+        
+    }
+}
